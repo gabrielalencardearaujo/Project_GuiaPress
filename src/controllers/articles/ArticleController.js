ead7ce98cb279d6dbf8607b065579ae8f0ac0132
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-01-11 23:15:56 -0300

    add tinymce for custom inputs as textarea

--- src/controllers/articles/ArticleController.js
+++ src/controllers/articles/ArticleController.js
@@ -1,7 +1,11 @@
 const Controllers = {
   home(req, res) {
     res.send('Pagina de articles.')
-  } 
+  },
+
+  new(req, res) {
+    res.render('admin/articles/new')
+  }
 }
 
 module.exports = Controllers;
