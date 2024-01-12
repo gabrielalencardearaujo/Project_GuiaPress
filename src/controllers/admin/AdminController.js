e6edb74b3036a82b2e8f2bbd5f8b7f286884a22a
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2024-01-11 22:35:17 -0300

    funcionality btn edit and delete

--- src/controllers/admin/AdminController.js
+++ src/controllers/admin/AdminController.js
@@ -3,7 +3,7 @@ const Slugify = require('slugify');
 
 const AdminController = {
   newCategory(req, res) {
-    res.render('admin/categories/new.ejs')
+    res.render('admin/categories/new.ejs', { category: null })
   },
 
   save(req, res) {
@@ -23,30 +23,61 @@ const AdminController = {
   },
 
   categories(req, res) {
-
     CategoryModel.findAll().then(categories => {
       res.render('admin/categories/index', {
         categories,
       })
-      
     })
   },
 
   delete(req, res) {
-    const {id} = req.body;
+    const { id } = req.body;
 
-    if(id && !isNaN(id)) {
+    if (id && !isNaN(id)) {
       //Deletar info no banco de dados que possua o mesmo id da variavel id:
       CategoryModel.destroy({
         where: {
           id: id
         }
-      }).then(()=> {
+      }).then(() => {
         res.redirect('/admin/categories/')
       })
 
     } else {
       res.redirect('/admin/categories/')
+    }
+  },
+
+  update(req, res) {
+    if (req.body.id) {
+      const { id, title } = req.body;
+
+      //Atualiza uma informacao no banco de dados:
+      CategoryModel.update({ title, slug: Slugify(title, { lower: true }) }, {
+        where: {
+          id,
+        }
+      })
+        .then(() => res.redirect('/admin/categories'))
+        .catch(err => res.redirect('/admin/categories'))
+
+    } else {
+
+      const { id } = req.params;
+
+      if (isNaN(id)) res.redirect('/admin/categories/');
+
+      CategoryModel.findByPk(id)
+        .then(category => {
+          if (category) {
+            res.render('admin/categories/new', { category: category })
+          } else {
+            res.redirect('/admin/categories/')
+          }
+        })
+        .catch(err => {
+          console.log('Ocorreu um erro ao buscar o id:', err)
+        })
     }
   }
 }
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const TestController = require('@controllers/controllers.js');
 const CateControllers = require('@controllers/categories/CategoriesController.js');
 const ArtControllers = require('@controllers/articles/ArticleController.js');
 const AdminController = require('@controllers/admin/AdminController.js');
+const ArticleModel = require('./controllers/articles/ArticleModel');
 
 // Routers Home
 router.get('/', TestController.helloWorld);
@@ -12,11 +13,14 @@ router.get('/categories', CateControllers.home);
 
 // Routers Articles
 router.get('/articles', ArtControllers.home);
+router.get('/admin/articles/new', ArtControllers.new)
 
 //Rotas Admin
 router.get('/admin/categories/', AdminController.categories);
 router.get('/admin/categories/new', AdminController.newCategory);
 router.post('/admin/categories/save', AdminController.save);
 router.post('/admin/categories/delete', AdminController.delete);
+router.get('/admin/categories/update/:id', AdminController.update);
+router.post('/admin/categories/update', AdminController.update);
 
 module.exports = router;
